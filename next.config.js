334edf562134782ac6da9a641d4523f9a32ec353
asil <asil9802mum@gmail.com>
2024-08-26 12:14:34 +0500

    update

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require('next-transpile-modules')(['@babel/preset-react']);
+
+module.exports = withTM({
+  reactStrictMode: true,
+  images: {
+    domains: ['flagcdn.com']
+  },
+  env: {
+    REACT_APP_VERSION: process.env.REACT_APP_VERSION,
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL
+  },
+  async redirects() {
+    // redirect - default first page should be `login` when root URL like http://example.com/
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true
+      }
+    ];
+  }
+});
